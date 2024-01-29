36619e015e89e87249bdc283fcc593a8d9adeb00
Santiago Brito <davidbri89@outlook.com>
2024-01-29 18:14:47 -0500

    FrontEnd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PrivateComponent } from './components/private/private.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: RegisterComponent },
+  { path: 'private', component: PrivateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PrivateComponent } from './components/private/private.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TermsComponent } from './components/terms/terms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    MenuComponent,
+    PrivateComponent,
+    FooterComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
